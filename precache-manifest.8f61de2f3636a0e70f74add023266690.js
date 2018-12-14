1ee7be92a2beb363afe99070c8104f9dce8d9ece
J Ko <just.jai.ko@gmail.com>
2018-12-14 14:30:32 -0800

    Deploying to GitHub.

---
+++ precache-manifest.8f61de2f3636a0e70f74add023266690.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e67df31d43bdbcd830c7",
+    "url": "/tile-shuffle/static/js/runtime~main.e67df31d.js"
+  },
+  {
+    "revision": "f7746fc6b5fe983e375f",
+    "url": "/tile-shuffle/static/js/main.f7746fc6.chunk.js"
+  },
+  {
+    "revision": "7071d84ada948cbdddf3",
+    "url": "/tile-shuffle/static/js/1.7071d84a.chunk.js"
+  },
+  {
+    "revision": "f7746fc6b5fe983e375f",
+    "url": "/tile-shuffle/static/css/main.f27567a9.chunk.css"
+  },
+  {
+    "revision": "9d4d9b1f3d6941ed07985d37518b2649",
+    "url": "/tile-shuffle/index.html"
+  }
+];
